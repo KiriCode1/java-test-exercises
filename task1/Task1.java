16bbef24dcc5bd3c1977294aa4f01a42c7741709
KiriCode1 <wearemusiciansmusic@gmail.com>
2023-08-03 14:24:02 +0300

    Init commit

---
+++ task1/Task1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class Task1 {
+	public static void main(String[] args) {
+		int n = Integer.parseInt(args[0]);
+
+		int m = Integer.parseInt(args[1]);
+
+		int[] circleArray = makeCircle(n);
+		makePath(n, m, circleArray);
+	}
+
+	private static int[] makeCircle(int len) {
+		int[] circle = new int[len];
+
+		for (int i = 0; i < len; i++) {
+			circle[i] = i + 1;
+		}
+
+		return circle;
+	}
+
+	private static void makePath(int len, int interval, int[] arr) {
+		int i = 1;
+
+		while (true) {
+			System.out.print(i);
+			i = 1 + (i + interval - 2) % len;
+
+			if (i == 1) break;
+		}
+		System.out.println();
+	}
+}
---
+++ task4/Task4.java
@@ -0,0 +1,33 @@
+class Task4 {
+	public static void main(String[] args) {
+		int avg = Math.round(sumArgs(args) / args.length);
+		int count = 0;
+
+		for (int i = 0; i <= args.length - 1; i++) {
+			int intArg = Integer.parseInt(args[i]);
+			while (intArg != avg) {
+				if (intArg < avg) {
+					intArg += 1;
+					count += 1;
+				} else if (intArg > avg) {
+					intArg -= 1;
+					count += 1;
+				} else {
+					args[i] = Integer.toString(intArg);
+				}
+			}
+		}
+
+		System.out.println("Количество шагов: " + Integer.toString(count));
+	}
+
+	private static int sumArgs(String[] ints) {
+		int res = 0;
+
+		for (int i = 0; i <= ints.length - 1; i++) {
+			res += Integer.parseInt(ints[i]);
+		}
+
+		return res;
+	}
+}
